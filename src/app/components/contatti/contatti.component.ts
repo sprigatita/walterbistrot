bce5c1bf788ed01dd9547ec02b3936c39f43974a
sprigatita <randazzomarta@icloud.com>
2025-06-04 15:50:42 +0200

    more stuff

--- src/app/components/contatti/contatti.component.ts
+++ src/app/components/contatti/contatti.component.ts
@@ -9,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class ContattiComponent {
 
+  bistroAddress : string = "https://www.google.com/maps/place/189+Castelnau,+London+SW13+9ER,+UK/@51.4853197,-0.2336257,17z/data=!3m1!4b1!4m6!3m5!1s0x48760fb2dd1e24b3:0xd283cd8801fe7162!8m2!3d51.4853197!4d-0.2336257!16s%2Fg%2F11c43vz62_?hl=en-gb&entry=ttu&g_ep=EgoyMDI1MDYwMS4wIKXMDSoASAFQAw%3D%3D"
+
 }
